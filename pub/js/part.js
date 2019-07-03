253a2a0923e4b8acb5c0d1d3d1beaba80eb5542b
chrisjd20 <chrisjd20@gmail.com>
2019-07-03 12:28:58 -0400

    Update part.js

--- pub/js/part.js
+++ pub/js/part.js
@@ -1,10 +1,10 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 160,
+      "value": 60,
       "density": {
         "enable": true,
-        "value_area": 800
+        "value_area": 1000
       }
     },
     "color": {
